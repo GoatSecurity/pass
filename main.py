11f12005a04c4e5e8bd22f50fae106eca5bedcb9
GoatSecurity <153397256+GoatSecurity@users.noreply.github.com>
2023-12-10 01:41:34 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+from itertools import permutations
+print('''
+ ___                                  _   ___                             _            
+| . \ ___  ___ ___ _ _ _  ___  _ _  _| | /  _>  ___ ._ _  ___  _ _  ___ _| |_ ___  _ _ 
+|  _/<_> |<_-<<_-<| | | |/ . \| '_>/ . | | <_/\/ ._>| ' |/ ._>| '_><_> | | | / . \| '_>
+|_|  <___|/__//__/|__/_/ \___/|_|  \___| `____/\___.|_|_|\___.|_|  <___| |_| \___/|_|  
+                                                                                        
+                             A Tool By : github.com/GoatSecurity
+
+
+''')
+def get_inputs():
+    inputs = []
+    while True:
+        inp = input("Type a word (leave blank to finish): ")
+        if inp == "":
+            break
+        inputs.append(inp)
+    return inputs
+
+def generate_passwords(inputs):
+    passwords = []
+    for r in range(1, len(inputs) + 1):
+        for permutation in permutations(inputs, r):
+            password = "".join(permutation)
+            passwords.append(password)
+    return passwords
+
+def write_passwords_to_file(passwords, filename):
+    with open(filename, 'w') as f:
+        for password in passwords:
+            f.write(password + '\n')
+
+inputs = get_inputs()
+passwords = generate_passwords(inputs)
+write_passwords_to_file(passwords, 'passwords.txt')
